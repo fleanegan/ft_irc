dcf49c1561ad6b38b46d20e7b01d41c2e3197141
AntoineLemarchand <antoine.lemarchand05@gmail.com>
2022-09-19 10:49:28 +0200

    feat(main.cpp): basic server

---
+++ inc/ircserv.hpp
@@ -0,0 +1,11 @@
+#ifndef IRCSERV_HPP
+# define IRCSERV_HPP
+
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#endif // IRCSERV_HPP
